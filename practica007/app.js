1c7eeea14a337cc723cc7dba7ac013e9afd57f74
enriqueao <enriqueao96@gmail.com>
2018-09-08 16:16:47 -0500

    se agregan nuevas practicas

---
+++ practica007/app.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+
+    res.writeHead(200, { 'Content-type': 'application/json' });
+
+    res.write(JSON.stringify({"Tony": "hola"}));
+
+    console.log(req.headers);
+    res.end();
+}).listen(3000, () => {
+    console.log("escuchando...");
+});
